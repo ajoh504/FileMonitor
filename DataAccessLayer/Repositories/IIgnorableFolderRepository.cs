6851b7aca43889c7141e5ef3802ad3acf5092f8e
ajoh504 <onegunnerunder@gmail.com>
2023-08-13 13:35:41 -0700

    Add IgnorableFolder repository interface

---
+++ DataAccessLayer/Repositories/IIgnorableFolderRepository.cs
@@ -0,0 +1,11 @@
+﻿using DataAccessLayer.Entities;
+
+namespace DataAccessLayer.Repositories
+{
+    /// <summary>
+    /// A repository interface for the IgnorableFolder entity.
+    /// </summary>
+    public interface IIgnorableFolderRepository : IRepository<IgnorableFolder>
+    {
+    }
+}
