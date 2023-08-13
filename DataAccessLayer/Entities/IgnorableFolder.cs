0390cea6310858f4d8557490c5dbf0cc7f05b8b2
ajoh504 <onegunnerunder@gmail.com>
2023-08-13 13:35:14 -0700

    Add IgnorableFolder entity - Used to ignore folders from being added to the program, such as folders that contain bin files.

---
+++ DataAccessLayer/Entities/IgnorableFolder.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataAccessLayer.Entities
+{
+    /// <summary>
+    /// An Entity class for the ignorable_folder table. These records store folder names that should be ignored by the program.
+    /// </summary>
+    [Table("ignorable_folder")]
+    public class IgnorableFolder
+    {
+        /// <summary>
+        /// The database primary key. 
+        /// </summary>
+        /// <remarks>
+        /// Use to remove Entities from the database.
+        /// </remarks>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The name of the folder/directory to be ignored.
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
