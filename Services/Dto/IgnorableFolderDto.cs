03f575d959d38b9970d49e1723ff506d3a1b974b
ajoh504 <onegunnerunder@gmail.com>
2023-08-19 15:48:12 -0700

    Add IgnorableFolderDto for updating the UI

---
+++ Services/Dto/IgnorableFolderDto.cs
@@ -0,0 +1,21 @@
+﻿namespace Services.Dto
+{
+    /// <summary>
+    /// A data transfer object related to the IgnorableFolder entity. Provides a folder name to be ignored by the program.
+    /// </summary>
+    public class IgnorableFolderDto
+    {
+        /// <summary>
+        /// The database primary key. 
+        /// </summary>
+        /// <remarks>
+        /// Use to remove Entities from the database.
+        /// </remarks>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The name of the folder/directory to be ignored.
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
