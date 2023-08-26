8573da4d9fbc16f6d334be5380144e612ea36434
ajoh504 <onegunnerunder@gmail.com>
2023-08-26 16:32:27 -0700

    Add IgnorableFolderHelper - Public static method to determine if a file is contained within an ignorable folder

---
+++ Services/Helpers/IgnorableFolderHelper.cs
@@ -0,0 +1,22 @@
+﻿using Services.Dto;
+
+namespace Services.Helpers
+{
+    /// <summary>
+    /// A static helper class for the <see cref="IgnorableFolderDto"/> and <see cref="IgnorableFolderService"/> classes.
+    /// </summary>
+    public static class IgnorableFolderHelper
+    {
+        /// <summary>
+        /// Returns false if the path is contained within an ignorable folder, true otherwise.
+        /// </summary>
+        public static bool NotIgnorable(string path, IEnumerable<IgnorableFolderDto> ignorableFolders)
+        {
+            foreach (IgnorableFolderDto folder in ignorableFolders)
+            {
+                if (path.Split(Path.DirectorySeparatorChar).Contains(folder.Name)) return false;
+            }
+            return true;
+        }
+    }
+}
