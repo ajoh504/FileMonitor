217501acf529bb0b530f2c7eff2f5e0604b63576
ajoh504 <onegunnerunder@gmail.com>
2023-10-14 08:37:50 -0700

    Add public interface to expose any data transfer object containing a folder or file path.

---
+++ Services/Dto/IPathDto.cs
@@ -0,0 +1,18 @@
+﻿namespace Services.Dto
+{
+    /// <summary>
+    /// A public interface to expose any data transfer object containing a folder or file path.
+    /// </summary>
+    public interface IPathDto
+    {
+        /// <summary>
+        /// Database ID.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Full path to the folder or file. 
+        /// </summary>
+        public string? Path { get; set; }
+    }
+}
