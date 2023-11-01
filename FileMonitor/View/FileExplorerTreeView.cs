9704a9fc2d6b4803eb90b418a2af2c6d8c57103c
ajoh504 <onegunnerunder@gmail.com>
2023-11-01 15:15:57 -0700

    Move PathNode into its own file

--- FileMonitor/View/FileExplorerTreeView.cs
+++ FileMonitor/View/FileExplorerTreeView.cs
@@ -156,29 +156,5 @@ namespace FileMonitor.View
             }
             return result;
         }
-
-        public class PathNode
-        {
-            public string? Text { get; set; }
-            public NodeCategory Category { get; set; }
-            public bool DisplayCheckBox { get; set; }
-            public int Id { get; set; }
-            public ObservableCollection<PathNode>? Children{ get; set; }
-
-            public override string? ToString() => Text;
-
-            public enum NodeCategory
-            {
-                Root = 0,
-                Directory = 1,
-                File = 2
-            }
-
-            public PathNode(string text, NodeCategory category)
-            {
-                Text = text;
-                Category = category;
-            }
-        }
     }
 }
---
+++ FileMonitor/View/PathNode.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileMonitor.View
+{
+    public class PathNode
+    {
+        public string? Text { get; set; }
+        public NodeCategory Category { get; set; }
+        public bool DisplayCheckBox { get; set; }
+        public int Id { get; set; }
+        public ObservableCollection<PathNode>? Children { get; set; }
+
+        public override string? ToString() => Text;
+
+        public enum NodeCategory
+        {
+            Root = 0,
+            Directory = 1,
+            File = 2
+        }
+
+        public PathNode(string text, NodeCategory category)
+        {
+            Text = text;
+            Category = category;
+        }
+    }
+}
