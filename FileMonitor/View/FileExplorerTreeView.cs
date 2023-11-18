aa123e8e0eee47c83b214be0d833cb2f6f1f3d36
ajoh504 <onegunnerunder@gmail.com>
2023-11-18 08:57:56 -0800

    Move PathNode class into FileExplorerTreeView. This class should be nested since the Nodes should not be instantiated outside of the TreeView

--- FileMonitor/View/FileExplorerTreeView.cs
+++ FileMonitor/View/FileExplorerTreeView.cs
@@ -178,5 +178,28 @@ namespace FileMonitor.View
             }
             return result;
         }
+
+        public class PathNode
+        {
+            public string? Text { get; set; }
+            public NodeCategory Category { get; set; }
+            public bool DisplayCheckBox { get; set; }
+            public ObservableCollection<PathNode>? Children { get; set; }
+
+            public override string? ToString() => Text;
+
+            public enum NodeCategory
+            {
+                Root = 0,
+                Directory = 1,
+                File = 2
+            }
+
+            public PathNode(string text)
+            {
+                Text = text;
+                Children = new ObservableCollection<PathNode>();
+            }
+        }
     }
 }
