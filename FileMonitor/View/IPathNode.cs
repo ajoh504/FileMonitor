ebefe926edb0e86cc736e36530c37414edc523f7
ajoh504 <onegunnerunder@gmail.com>
2023-11-30 06:34:52 -0800

    Create IPathNode interface to publicly expose PathNode data

---
+++ FileMonitor/View/IPathNode.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.ObjectModel;
+
+namespace FileMonitor.View
+{
+    interface IPathNode
+    {
+        public string? Text { get; set; }
+        public FileExplorerTreeView.NodeCategory Category { get; set; }
+        public bool DisplayCheckBox { get; set; }
+        public ObservableCollection<IPathNode>? Children { get; set; }
+        public int PathId { get; set; }
+    }
+}
